ee9f3bad3e4f71085c09499e0e5e6706ebc1b1d1
Idan Lupinsky <idan.lupinsky@gmail.com>
2021-05-14 07:43:59 +0200

    add basic setup

--- test/article-code-ecs-service-auto-scaling-2.test.ts
+++ test/article-code-ecs-service-auto-scaling-2.test.ts
@@ -1,13 +1,13 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as ArticleCodeEcsServiceAutoScaling2 from '../lib/article-code-ecs-service-auto-scaling-2-stack';
+import * as EcsServiceAutoScaling from '../lib/ecs-service-auto-scaling-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new ArticleCodeEcsServiceAutoScaling2.ArticleCodeEcsServiceAutoScaling2Stack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new EcsServiceAutoScaling.EcsServiceAutoScalingStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT))
 });
